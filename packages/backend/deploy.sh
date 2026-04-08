d5f0da23f481e57df3890e8dee5b92fbbead90a4
elijahnikov <enkv@proton.me>
2026-04-08 17:15:44 +0100

    fix

--- packages/backend/deploy.sh
+++ packages/backend/deploy.sh
@@ -4,5 +4,5 @@ CMD="cd ../../apps/app && turbo run build"
 if [ "$VERCEL_ENV" = "production" ]; then
   bunx convex deploy --cmd "$CMD" --cmd-url-env-var-name VITE_CONVEX_URL
 else
-  bunx convex deploy --preview "$VERCEL_GIT_COMMIT_REF" --cmd "$CMD" --cmd-url-env-var-name VITE_CONVEX_URL
+  bunx convex deploy --preview-create "$VERCEL_GIT_COMMIT_REF" --cmd "$CMD" --cmd-url-env-var-name VITE_CONVEX_URL
 fi
